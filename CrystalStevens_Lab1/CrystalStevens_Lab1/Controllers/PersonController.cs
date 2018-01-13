cb4a0b19e0d0a0b3f251b8ea56199504fc67d00e
CrystalMacDonald <crystal.m.stevens@gmail.com>
2018-01-13 11:56:22 -0800

    Added Lab 1 Docs
    Add Project and answers to questions for Lab 1

---
+++ CrystalStevens_Lab1/CrystalStevens_Lab1/Controllers/PersonController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrystalStevens_Lab1.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This is some data about a person. It should be displayed now!!";
+            return View();
+        }
+    }
+}
