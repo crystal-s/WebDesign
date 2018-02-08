e15c9f04c698dc8b5b8da95321d1f150f685bdd0
CrystalMacDonald <crystal.m.stevens@gmail.com>
2018-02-07 17:25:38 -0800

    Revised Lab 3
    This is the revised, working version of Lab 3.

---
+++ CrystalStevensLab3Week4/CrystalStevensLab3Week4/Controllers/PetController.cs
@@ -0,0 +1,127 @@
+﻿using CrystalStevensLab3Week4.Data;
+using CrystalStevensLab3Week4.Data.Entities;
+using CrystalStevensLab3Week4.Models.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrystalStevensLab3Week4.Controllers
+{
+    public class PetController : Controller
+    {
+        public ActionResult List(int userId)
+        {
+            ViewBag.UserId = userId;
+
+            var pets = GetPetsForUser(userId);
+
+            return View(pets);
+        }
+
+        [HttpGet]
+        public ActionResult Create(int userId)
+        {
+            ViewBag.UserId = userId;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(PetViewModel petViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Save(petViewModel);
+                return RedirectToAction("List", new { UserId = petViewModel.UserId });
+            }
+
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var pet = GetPet(id);
+
+            DeletePet(id);
+
+            return RedirectToAction("List", new { UserId = pet.UserId });
+        }
+
+        private Pet GetPet(int petId)
+        {
+            var dbContext = new AppDbContext();
+
+            return dbContext.Pets.Find(petId);
+        }
+
+        private ICollection<PetViewModel> GetPetsForUser(int userId)
+        {
+            var petViewModels = new List<PetViewModel>();
+
+            var dbContext = new AppDbContext();
+
+            var pets = dbContext.Pets.Where(pet => pet.UserId == userId).ToList();
+
+            foreach (var pet in pets)
+            {
+                var petViewModel = MapToPetViewModel(pet);
+                petViewModels.Add(petViewModel);
+            }
+
+            return petViewModels;
+        }
+
+        private void Save(PetViewModel petViewModel)
+        {
+            var dbContext = new AppDbContext();
+
+            var pet = MapToPet(petViewModel);
+
+            dbContext.Pets.Add(pet);
+
+            dbContext.SaveChanges();
+        }
+
+        private void DeletePet(int id)
+        {
+            var dbContext = new AppDbContext();
+
+            var pet = dbContext.Pets.Find(id);
+
+            if (pet != null)
+            {
+                dbContext.Pets.Remove(pet);
+                dbContext.SaveChanges();
+            }
+        }
+
+        private Pet MapToPet(PetViewModel petViewModel)
+        {
+            return new Pet
+            {
+                Id = petViewModel.Id,
+                Name = petViewModel.Name,
+                Age = petViewModel.Age,
+                Chipped = petViewModel.Chipped,
+                NextCheckup = petViewModel.NextCheckup,
+                VetName = petViewModel.VetName,
+                UserId = petViewModel.UserId
+            };
+        }
+
+        private PetViewModel MapToPetViewModel(Pet pet)
+        {
+            return new PetViewModel
+            {
+                Id = pet.Id,
+                Name = pet.Name,
+                Age = pet.Age,
+                NextCheckup = pet.NextCheckup,
+                VetName = pet.VetName,
+                UserId = pet.UserId
+            };
+        }
+    }
+}
